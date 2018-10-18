c60f3da7c8eb9246430186826f1981f2464d379f
Lyaction <1572946585@qq.com>
2018-10-18 19:43:54 +0800

    1:重写了Mapper中的map方法 2:了解了mapreduce的IO输入相关的类和方法

--- src/main/java/courses/hadoop/model/BayesModel.java
+++ src/main/java/courses/hadoop/model/BayesModel.java
@@ -1,5 +1,35 @@
 package courses.hadoop.model;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
 public class BayesModel {
 
+	public static class TokenizerMapper 
+			extends Mapper<Object, Text, Text, IntWritable>{
+		
+		private final static IntWritable  One = new IntWritable(1);
+		private Text word = new Text();
+		
+		public void map(Object key, Text value, Context context
+					)throws IOException, InterruptedException {
+			StringTokenizer iTokenizer = new StringTokenizer(value.toString());
+			while(iTokenizer.hasMoreTokens()) {
+				word.set(iTokenizer.nextToken());
+				context.write(word, One);
+			}
+		}
+	}
+	
+	public static class IntSumReducer
+			extends Reducer<Text, IntWritable, Text, IntWritable>{
+		
+		
+	}
 }
